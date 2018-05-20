9d71dcf2bd7884b2c3e50f4bd97119c08e72c7f1
Mehreen Butt <mbutt@republiccharterschools.org>
2018-05-20 17:33:40 +0000

    Done.

---
+++ beyonce.rb
@@ -0,0 +1,6 @@
+def change_my_name
+  name = "Frank"
+  puts name.reverse
+  return name
+end
+6+3
---
+++ prosecco.rb
@@ -0,0 +1,14 @@
+for k in 1.99 do 
+   puts "Number #{k}" 
+end
+
+99.times do |k|
+   puts "Number #{k-1}"   
+end
+
+puts "99 bottles of beer on the wall, 99 bottles of beer.
+Take one down and pass it around, 98 bottles of beer on the wall."
+
+puts "Take one down and pass it around, no more bottles of beer on the wall.
+No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall."
